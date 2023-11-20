84d933e8065f5f538f228a8e7647e40f7f9d29a9
suhashstudy <suhashstudy@gmail.com>
2023-11-20 11:38:24 +0530

    removed index and added is not none condition

--- Git_module_1.py
+++ Git_module_1.py
@@ -43,22 +43,23 @@ def calling_function():
     branches = get_branches(repo)
 
     if branches:
-        selected_branch = st.selectbox("Select branch:", branches)
+        selected_branch = st.selectbox("Select branch:", branches,)
 
         folders = get_folders(repo, selected_branch)
 
-        if folders:
-            selected_folder = st.selectbox("Select folder:", folders)
+        if folders and selected_branch  is not None :
+            selected_folder = st.selectbox("Select folder:", folders,)
 
             files = get_files(repo, selected_branch, selected_folder)
 
-            if files:
-                selected_file = st.selectbox("Select file:", files)
+            if files and selected_folder  is not None :
+                selected_file = st.selectbox("Select file:", files,)
 
                 file_content = get_file_content(repo, selected_branch, selected_folder + '/' + selected_file)
 
                 if file_content:
-                    st.write(f"Content of {selected_folder}/{selected_file}:\n{file_content}")
+                    st.write(f"Content of {selected_folder}/{selected_file}:")
+                    st.code(file_content, language='python')
             else:
                 st.warning("No files found in the selected folder.")
         else:
